ce0f51a1844d7d42004374ce29489e8b4072079a
Kevin Thulnith <112649346+kevinThulnith@users.noreply.github.com>
2025-09-01 23:09:40 +0530

    20th commit

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import App from "./App";
 import "./index.css";
 
-// Use createRoot for client-side only rendering
+// !Use createRoot for client-side only rendering
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -2,6 +2,7 @@ import ReactDOMServer from "react-dom/server";
 import React from "react";
 import App from "./App";
 
+// !Use renderToString for server-side rendering
 export function render() {
   const html = ReactDOMServer.renderToString(
     <React.StrictMode>
